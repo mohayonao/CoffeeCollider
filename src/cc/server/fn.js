89d9e7d159612aae8ed3e944a0d8c904f3074be3
mohayonao <mohayonao@gmail.com>
2013-10-10 15:46:52 +0900

    Added utility functions

---
+++ src/cc/server/fn.js
@@ -0,0 +1,106 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var slice = [].slice;
+  
+  var fn = (function() {
+    function Fn(func) {
+      this.func = func;
+      this.def  = "";
+    }
+    Fn.prototype.defaults = function(def) {
+      this.def = def;
+      return this;
+    };
+    Fn.prototype.build = function() {
+      var func = this.func;
+      var keys = [];
+      var vals = [];
+      this.def.split(",").forEach(function(items) {
+        items = items.trim().split("=");
+        keys.push( items[0].trim());
+        vals.push(+items[1].trim());
+      });
+      var ret = func;
+      if (this.def !== "") {
+        ret = function() {
+          return func.apply(this, resolve_args(keys, vals, slice.call(arguments)));
+        };
+      }
+      return ret;
+    };
+    var resolve_args = function(keys, vals, given) {
+      var dict;
+      var args = vals.slice();
+      if (fn.isDictionary(given[given.length - 1])) {
+        dict = given.pop();
+        for (var key in dict) {
+          var index = keys.indexOf(key);
+          if (index !== -1) {
+            args[index] = dict[key];
+          }
+        }
+      }
+      for (var i = 0, imax = Math.min(given.length, args.length); i < imax; ++i) {
+        args[i] = given[i];
+      }
+      if (dict && args.length < keys.length - 1) {
+        args.push(dict);
+      }
+      return args;
+    };
+    return function(func) {
+      return new Fn(func);
+    };
+  })();
+
+  fn.extend = function(child, parent) {
+    for (var key in parent) {
+      if (parent.hasOwnProperty(key)) {
+        child[key] = parent[key];
+      }
+    }
+    /*jshint validthis:true */
+    function ctor() {
+      this.constructor = child;
+    }
+    /*jshint validthis:false */
+    ctor.prototype = parent.prototype;
+    /*jshint newcap:false */
+    child.prototype = new ctor();
+    /*jshint newcap:true */
+    child.__super__ = parent.prototype;
+    return child;
+  };
+
+  fn.classmethod = (function() {
+    var _classmethod = function(Klass, func) {
+      return function() {
+        return func.apply(new Klass(), arguments);
+      };
+    };
+    return function(child) {
+      var classmethods = child.classmethods || {};
+      Object.keys(child.prototype).forEach(function(key) {
+        if (key.charAt(0) === "$" && typeof child.prototype[key] === "function") {
+          classmethods[key] = child.prototype[key];
+          delete child.prototype[key];
+        }
+      });
+      Object.keys(classmethods).forEach(function(key) {
+        var func = classmethods[key];
+        key = key.substr(1);
+        child[key] = _classmethod(child, func);
+        child.prototype[key] = func;
+      });
+      child.classmethods = classmethods;
+    };
+  })();
+  
+  fn.isDictionary = function(obj) {
+    return !!(obj && obj.constructor === Object);
+  };
+
+  module.exports = fn;
+
+});
---
+++ src/cc/server/fn_test.js
@@ -0,0 +1,68 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var assert = require("chai").assert;
+  var fn = require("./fn");
+
+  describe("fn", function() {
+    var Foo, Bar;
+    before(function() {
+      Foo = (function() {
+        function Foo() {}
+        Foo.prototype.$create = function(value) {
+          return this.setValue(value);
+        };
+        Foo.prototype.setValue = function(value) {
+          this.value = value;
+          return this;
+        };
+        fn.classmethod(Foo);
+        return Foo;
+      })();
+      Bar = (function() {
+        function Bar() {}
+        fn.extend(Bar, Foo);
+        Bar.prototype.setValue = function(value) {
+          this.value = value * 100;
+          return this;
+        };
+        fn.classmethod(Bar);
+        return Bar;
+      })();
+    });
+    describe("Fn", function() {
+      it("defaults", function() {
+        var madd = fn(function(val, mul, add) {
+          return val * mul + add;
+        }).defaults("val=0,mul=1,add=0").build();
+        assert.equal(10, madd(10));
+        assert.equal(20, madd(10, 2));
+        assert.equal(30, madd(10, {add:20}));
+      });
+    });
+    it("extend", function() {
+      assert.instanceOf(new Bar(), Foo);
+    });
+    describe("classmethod", function() {
+      it("create", function() {
+        var f = Foo.create(1000);
+        assert.instanceOf(f, Foo);
+        assert.equal(f.value, 1000);
+      });
+      it("extend", function() {
+        var b = Bar.create(1000);
+        assert.instanceOf(b, Bar);
+        assert.instanceOf(b, Foo);
+        assert.equal(b.value, 1000 * 100);
+      });
+    });
+    it("isDictionary", function() {
+      var __ = new (function() {})();
+      assert.equal(true , fn.isDictionary({}));
+      assert.equal(false, fn.isDictionary([]));
+      assert.equal(false, fn.isDictionary(__));
+      assert.equal(false, fn.isDictionary(null));
+    });
+  });
+  
+});
