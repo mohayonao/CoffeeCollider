7076b3990a8342d1e41d428b8cb6f3862e954880
mohayonao <mohayonao@gmail.com>
2013-10-10 12:07:56 +0900

    Updated arithmetic_test

--- src/cc/test/arithmetic_test.js
+++ src/cc/test/arithmetic_test.js
@@ -41,18 +41,17 @@ define(function(require, exports, module) {
       var actual = calc(expr);
       assert.equal(actual, expected);
     });
-    it("1000 + [1, 2, 3]", function() {
-      var expr = "1000 + [1, 2, 3]";
-      var expected = [ 1001, 1002, 1003 ];
+    it("1000 + [1, 2, 3] * 111", function() {
+      var expr = "1000 + [1, 2, 3] * 111";
+      var expected = [ 1000 + 1 * 111, 1000 + 2 * 111, 1000 + 3 * 111];
       var actual = calc(expr);
       assert.deepEqual(actual, expected);
     });
     it("1 + +!![0..5]", function() {
       var expr = "1 + +!![0..5]";
-      var expected = [ 1, 2, 2, 2, 2, 2 ];
+      var expected = [ 1 + +!!0, 1 + +!!1, 1 + +!!2, 1 + +!!3, 1 + +!!4, 1 + +!!5 ];
       var actual = calc(expr);
       assert.deepEqual(actual, expected);
     });
   });
-  
 });
