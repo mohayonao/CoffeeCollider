728f72e596248a1eff2ac5e7b08018d7f0716b4f
mohayonao <mohayonao@gmail.com>
2013-10-15 10:46:21 +0900

    Refactored bop.js

--- src/cc/server/bop.js
+++ src/cc/server/bop.js
@@ -4,98 +4,89 @@ define(function(require, exports, module) {
   var UGen = require("./ugen/ugen").UGen;
   var BinaryOpUGen = require("./ugen/basic_ops").BinaryOpUGen;
 
-  var aliases = {
-    __add__: "+",
-    __sub__: "-",
-    __mul__: "*",
-    __div__: "/",
-    __mod__: "%",
+  var setupNumberFunction = function(func, selector, ugenSelector) {
+    return function(b) {
+      if (Array.isArray(b)) {
+        return b.map(function(b) {
+          return this[selector](b);
+        }, this);
+      } else if (b instanceof UGen) {
+        return BinaryOpUGen.new(ugenSelector, this, b);
+      }
+      return func(this, b);
+    };
+  };
+  var setupArrayFunction = function(selector, ugenSelector) {
+    return function(b) {
+      var a = this;
+      if (Array.isArray(b)) {
+        if (a.length === b.length) {
+          return a.map(function(a, index) {
+            return a[selector](b[index]);
+          });
+        } else if (a.length > b.length) {
+          return a.map(function(a, index) {
+            return a[selector](b[index % b.length]);
+          });
+        } else {
+          return b.map(function(b, index) {
+            return a[index % a.length][selector](b);
+          });
+        }
+      } else if (b instanceof UGen) {
+        return BinaryOpUGen.new(ugenSelector, this, b);
+      }
+      return a.map(function(a) {
+        return a[selector](b);
+      });
+    };
+  };
+  var setupUGenFunction = function(selector) {
+    return function(b) {
+      return BinaryOpUGen.new(selector, this, b);
+    };
+  };
+
+  var setup = function(selector, func, ugenSelector) {
+    ugenSelector = ugenSelector || selector;
+    Number.prototype[selector] = setupNumberFunction(func, selector, ugenSelector);
+    Array.prototype[selector]  = setupArrayFunction(selector, ugenSelector);
+    UGen.prototype[selector]   = setupUGenFunction(ugenSelector);
   };
 
   var install = function() {
-    Object.keys(calcFunc).forEach(function(key) {
-      var keyForBop = aliases[key] || key;
-      var func = calcFunc[key];
-      Number.prototype[key] = function(b) {
-        if (Array.isArray(b)) {
-          return b.map(function(b) {
-            return this[key](b);
-          }, this);
-        } else if (b instanceof UGen) {
-          return BinaryOpUGen.new(keyForBop, this, b);
-        }
-        return func(this, b);
-      };
-      if (calcFunc[key].array) {
-        func = calcFunc[key];
-      }
-      Array.prototype[key] = function(b) {
-        var a = this;
-        if (Array.isArray(b)) {
-          if (a.length === b.length) {
-            return a.map(function(a, index) {
-              return a[key](b[index]);
-            });
-          } else if (a.length > b.length) {
-            return a.map(function(a, index) {
-              return a[key](b[index % b.length]);
-            });
-          } else {
-            return b.map(function(b, index) {
-              return a[index % a.length][key](b);
-            });
-          }
-        } else if (b instanceof UGen) {
-          return BinaryOpUGen.new(keyForBop, this, b);
-        }
-        return a.map(function(a) {
-          return a[key](b);
-        });
-      };
-      UGen.prototype[key] = function(b) {
-        return BinaryOpUGen.new(keyForBop, this, b);
-      };
-      if (calcFunc[key].str) {
-        var strFunc = calcFunc[key].str;
-        String.prototype[key] = function(b) {
-          if (Array.isArray(b)) {
-            return b.map(function(b) {
-              return this[key](b);
-            }, this);
-          }
-          return strFunc(this, b);
-        };
-      }
-    });
-  };
+    setup("__add__", function(a, b) {
+      return a + b;
+    }, "+");
+    String.prototype.__add__ = function(b) {
+      return this + b;
+    };
 
-  var calcFunc = {};
-
-  calcFunc.__add__ = function(a, b) {
-    return a + b;
-  };
-  calcFunc.__add__.str = calcFunc.__add__;
-  calcFunc.__sub__ = function(a, b) {
-    return a - b;
-  };
-  calcFunc.__mul__ = function(a, b) {
-    return a * b;
-  };
-  calcFunc.__mul__.str = function(a, b) {
-    if (typeof b === "number") {
-      var list = new Array(Math.max(0, b));
-      for (var i = 0; i < b; i++) {
-        list[i] = a;
+    setup("__sub__", function(a, b) {
+      return a - b;
+    }, "-");
+    
+    setup("__mul__", function(a, b) {
+      return a * b;
+    }, "*");
+    String.prototype.__mul__ = function(b) {
+      if (typeof b === "number") {
+        var result = new Array(Math.max(0, b));
+        for (var i = 0; i < b; i++) {
+          result[i] = this;
+        }
+        return result.join("");
       }
-      return list.join("");
-    }
-    return a;
-  };
-  calcFunc.__div__ = function(a, b) {
-    return a / b;
-  };
-  calcFunc.__mod__ = function(a, b) {
-    return a % b;
+      return this;
+    };
+
+    setup("__div__", function(a, b) {
+      return a / b;
+    }, "/");
+
+    setup("__mod__", function(a, b) {
+      return a % b;
+    }, "%");
   };
   
   module.exports = {
--- src/cc/server/bop_test.js
+++ src/cc/server/bop_test.js
@@ -4,85 +4,85 @@ define(function(require, exports, module) {
   var assert = require("chai").assert;
   var bop = require("./bop");
 
-  describe("bop:", function() {
+  describe("bop.js", function() {
     before(function() {
       bop.install();
     });
     describe("+", function() {
-      it("(num + num)", function() {
+      it("(num + num) => num", function() {
         var actual = 1..__add__(10);
         var expected = 1 + 10;
         assert.equal(actual, expected);
       });
-      it("(num + ary)", function() {
+      it("(num + ary) => ary", function() {
         var actual = 1..__add__([10, 20, 30]);
         var expected = [1+10, 1+20, 1+30];
         assert.deepEqual(actual, expected);
       });
-      it("(ary + ary)", function() {
+      it("(ary + ary) => ary", function() {
         var actual = [1, 2, 3].__add__([10, 20, 30]);
         var expected = [1+10, 2+20, 3+30];
         assert.deepEqual(actual, expected);
       });
-      it("(ary + num)", function() {
+      it("(ary + num) => ary", function() {
         var actual = [1, 2, 3].__add__(10);
         var expected = [1+10, 2+10, 3+10];
         assert.deepEqual(actual, expected);
       });
-      it("(str + str)", function() {
+      it("(str + str) => str", function() {
         var actual = "abc".__add__("123");
         var expected = "abc" + "123";
         assert.equal(actual, expected);
       });
     });
     describe("-", function() {
-      it("(num - num)", function() {
+      it("(num - num) => num", function() {
         var actual = 1..__sub__(10);
         var expected = 1 - 10;
         assert.equal(actual, expected);
       });
-      it("(ary - num)", function() {
+      it("(ary - num) => ary", function() {
         var actual = [1, 2, 3].__sub__(10);
         var expected = [1-10, 2-10, 3-10];
         assert.deepEqual(actual, expected);
       });
     });
     describe("*", function() {
-      it("(num * num)", function() {
+      it("(num * num) => num", function() {
         var actual = 1..__mul__(10);
         var expected = 1 * 10;
         assert.equal(actual, expected);
       });
-      it("(ary * num)", function() {
+      it("(ary * num) => ary", function() {
         var actual = [1, 2, 3].__mul__(10);
         var expected = [1*10, 2*10, 3*10];
         assert.deepEqual(actual, expected);
       });
-      it("(str * num)", function() {
+      it("(str * num) => str", function() {
         var actual = "abc".__mul__(3);
         var expected = "abc" + "abc" + "abc";
         assert.equal(actual, expected);
       });
     });
     describe("/", function() {
-      it("(num / num)", function() {
+      it("(num / num) => num", function() {
         var actual = 1..__div__(10);
         var expected = 1 / 10;
         assert.equal(actual, expected);
       });
-      it("(ary / num)", function() {
+      it("(ary / num) => ary", function() {
         var actual = [1, 2, 3].__div__(10);
         var expected = [1/10, 2/10, 3/10];
         assert.deepEqual(actual, expected);
       });
     });
     describe("%", function() {
-      it("(num % num)", function() {
+      it("(num % num) => num", function() {
         var actual = 1..__mod__(10);
         var expected = 1 % 10;
         assert.equal(actual, expected);
       });
-      it("(ary % num)", function() {
+      it("(ary % num) => ary", function() {
         var actual = [1, 2, 3].__mod__(10);
         var expected = [1%10, 2%10, 3%10];
         assert.deepEqual(actual, expected);
