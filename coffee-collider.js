7cbb760091eadd4814cdd6e117f674fac9d0a85d
mohayonao <mohayonao@gmail.com>
2013-11-01 13:16:02 +0900

    Fixed specialIndexes of unary/binary operators

--- coffee-collider.js
+++ coffee-collider.js
@@ -1998,8 +1998,8 @@ define('cc/client/ugen/uop', function(require, exports, module) {
 });
 define('cc/common/ops', function(require, exports, module) {
 
-  var UNARY_OP_UGEN_MAP = "num neg not tilde pi abs midicps cpsmidi ampdb dbamp coin".split(" ");
-  var BINARY_OP_UGEN_MAP = "+ - * / %".split(" ");
+  var UNARY_OP_UGEN_MAP = "neg not isNil notNil bitNot abs asFloat asInt ceil floor frac sign squared cubed sqrt exp reciprocal midicps cpsmidi midiratio ratiomidi dbamp ampdb octcps cpsoct log log2 log10 sin cos tan asin acos atan sinh cosh tanh rand rand2 linrand bilinrand sum3rand distort softclip coin digitvalue silence thru rectWindow hanWindow welWindow triWindow ramp scurve numunaryselectors num tilde pi".split(" ");
+  var BINARY_OP_UGEN_MAP = "+ - * / / % == != < > <= >= min max & | ^ lcm gcd round roundUp trunc atan2 hypot hypotApx pow << >> >>> fill ring1 ring2 ring3 ring4 difsqr sumsqr sqrsum sqrdif absdif thresh amclip scaleneg clip2 excess fold2 wrap2 firstarg randrange exprandrange numbinaryselectors".split(" ");
   
   module.exports = {
     UNARY_OP_UGEN_MAP : UNARY_OP_UGEN_MAP,
