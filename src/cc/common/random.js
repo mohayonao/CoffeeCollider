0e96c8a20f21afb512b43b5cd823755f1c2fbcec
mohayonao <mohayonao@gmail.com>
2013-11-22 22:10:13 +0900

    Fixed random generator

--- src/cc/common/random.js
+++ src/cc/common/random.js
@@ -6,33 +6,34 @@ define(function(require, exports, module) {
       if (typeof seed !== "number") {
         seed = Date.now();
       }
-      var hash = seed;
-      hash += ~(hash <<  15);
-      hash ^=   hash >>> 10;
-      hash +=   hash <<  3;
-      hash ^=   hash >>> 6;
-      hash += ~(hash <<  11);
-      hash ^=   hash >>> 16;
+      seed = new Uint32Array([seed]);
+      seed[0] += ~(seed[0] <<  15);
+      seed[0] ^=   seed[0] >>> 10;
+      seed[0] +=   seed[0] <<  3;
+      seed[0] ^=   seed[0] >>> 6;
+      seed[0] += ~(seed[0] <<  11);
+      seed[0] ^=   seed[0] >>> 16;
       
-      this.s1 = 1243598713 ^ seed;
-      this.s2 = 3093459404 ^ seed;
-      this.s3 = 1821928721 ^ seed;
-      if (this.s1 <  2) {
-        this.s1 = 1243598713;
+      this.s1 = new Uint32Array([1243598713 ^ seed[0]]);
+      this.s2 = new Uint32Array([3093459404 ^ seed[0]]);
+      this.s3 = new Uint32Array([1821928721 ^ seed[0]]);
+      
+      if (this.s1[0] <  2) {
+        this.s1[0] = 1243598713;
       }
-      if (this.s2 <  8) {
-        this.s2 = 3093459404;
+      if (this.s2[0] <  8) {
+        this.s2[0] = 3093459404;
       }
-      if (this.s3 < 16) {
-        this.s3 = 1821928721;
+      if (this.s3[0] < 16) {
+        this.s3[0] = 1821928721;
       }
     }
     
     Random.prototype.trand = function() {
-      this.s1 = ((this.s1 & 4294967294) << 12) ^ (((this.s1 << 13) ^  this.s1) >>> 19);
-      this.s2 = ((this.s2 & 4294967288) <<  4) ^ (((this.s2 <<  2) ^  this.s2) >>> 25);
-      this.s3 = ((this.s3 & 4294967280) << 17) ^ (((this.s3 <<  3) ^  this.s3) >>> 11);
-      return this.s1 ^ this.s2 ^ this.s3;
+      this.s1[0] = ((this.s1[0] & 4294967294) << 12) ^ (((this.s1[0] << 13) ^  this.s1[0]) >>> 19);
+      this.s2[0] = ((this.s2[0] & 4294967288) <<  4) ^ (((this.s2[0] <<  2) ^  this.s2[0]) >>> 25);
+      this.s3[0] = ((this.s3[0] & 4294967280) << 17) ^ (((this.s3[0] <<  3) ^  this.s3[0]) >>> 11);
+      return this.s1[0] ^ this.s2[0] ^ this.s3[0];
     };
     
     var _i = new Uint32Array(1);
--- src/cc/common/random_test.js
+++ src/cc/common/random_test.js
@@ -6,6 +6,7 @@ define(function(require, exports, module) {
   var Random = require("./random").Random;
   
   describe("lang/random.js", function() {
+    var actual, expected;
     describe("Random", function() {
       it("create", function() {
         var r = new Random();
@@ -30,6 +31,26 @@ define(function(require, exports, module) {
         var r2 = new Random(1821928721);
         assert.notEqual(r1.next(), r2.next());
         assert.notEqual(r1.next(), r2.next());
+      });
+      it("trand", function() {
+        var r = new Random(1923);
+        actual = [];
+        for (var i = 0; i < 10; ++i) {
+          actual.push(r.trand());
+        }
+        expected = [ 1434734233, -365758490, -1302464995, 1866865983, 1820400704,
+                     2118393990, 757590367, 2051568998, 1510405633, 2022411921 ];
+        assert.deepEqual(actual, expected);
+      });
+      it("next", function() {
+        var r = new Random(1923);
+        actual = [];
+        for (var i = 0; i < 10; ++i) {
+          actual.push(r.next());
+        }
+        expected = [ 0.334050094941631, 0.914840215351433, 0.696746236877516, 0.434663608437404, 0.423845067620277,
+                     0.493227036204189, 0.176390252774581, 0.477668130304664, 0.351668715709820, 0.470879469299689 ];
+        assert.deepCloseTo(actual, expected, 1e-6);
       });
     });
   });
