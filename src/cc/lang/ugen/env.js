1ac68200db3ab53338bc7aeb2569f77b8ae049d0
mohayonao <mohayonao@gmail.com>
2013-11-24 21:26:33 +0900

    Added EnvGen

--- src/cc/lang/ugen/env.js
+++ src/cc/lang/ugen/env.js
@@ -3,6 +3,27 @@ define(function(require, exports, module) {
 
   var cc = require("../cc");
 
+  var convertEnv = function(env) {
+    return env.asMultichannelArray();
+  };
+  
+  cc.ugen.specs.EnvGen = {
+    // $ar: {
+    //   defaults: "envelope,gate=1,levelScale=1,levelBias=0,timeScale=1,doneAction=0",
+    //   ctor: function(envelope, gate, levelScale, levelBias, timeScale, doneAction) {
+    //     envelope = convertEnv(envelope)[0]; // TODO: unbubble
+    //     return this.init.apply(this, [C.AUDIO, gate, levelScale, levelBias, timeScale, doneAction].concat(envelope));
+    //   }
+    // },
+    $kr: {
+      defaults: "envelope,gate=1,levelScale=1,levelBias=0,timeScale=1,doneAction=0",
+      ctor: function(envelope, gate, levelScale, levelBias, timeScale, doneAction) {
+        envelope = convertEnv(envelope)[0]; // TODO: unbubble
+        return this.init.apply(this, [C.CONTROL, gate, levelScale, levelBias, timeScale, doneAction].concat(envelope));
+      }
+    }
+  };
+  
   cc.ugen.specs.Linen = {
     $kr: {
       defaults: "gate=1,attackTime=0.01,susLevel=1,releaseTime=1,doneAction=0",
--- src/cc/server/unit/env_test.js
+++ src/cc/server/unit/env_test.js
@@ -1,5 +1,36 @@
 define(function(require, exports, module) {
   "use strict";
+
+  var unitTestSuite = require("../../../testTools").unitTestSuite;
+  var unit = require("./unit");
+  var env  = require("./env");
+
+  unitTestSuite.desc = "server/unit/env.js";
+  
+  unitTestSuite("EnvGen", [
+    { rate  : C.AUDIO,
+      inputs: [
+        { name:"gate"      , rate:C.SCALAR, value:1 },
+        { name:"levelScale", rate:C.SCALAR, value:1 },
+        { name:"levelBias" , rate:C.SCALAR, value:0 },
+        { name:"timeScale" , rate:C.SCALAR, value:1 },
+        { name:"doneAction", rate:C.SCALAR, value:0 },
+        
+        { rate:C.SCALAR, value: 0.0 },
+        { rate:C.SCALAR, value: 2.0 },
+        { rate:C.SCALAR, value: -99 },
+        { rate:C.SCALAR, value: -99 },
+        { rate:C.SCALAR, value: 1.0 },
+        { rate:C.SCALAR, value: 0.5 },
+        { rate:C.SCALAR, value: 3.0 },
+        { rate:C.SCALAR, value: 0.0 },
+        { rate:C.SCALAR, value: 0.0 },
+        { rate:C.SCALAR, value: 0.5 },
+        { rate:C.SCALAR, value: 3.0 },
+        { rate:C.SCALAR, value: 0.0 },
+      ]
+    }
+  ]);
   
   module.exports = {};
 
