edd0e6164f9e5ffc0bb394be5328e85c0743052c
mohayonao <mohayonao@gmail.com>
2013-12-14 21:55:00 +0900

    Add the -e option to the cli

--- bin/coffeecollider.js
+++ bin/coffeecollider.js
@@ -7,9 +7,10 @@
   var optparse = require("optparse");
   
   var SWITCHES = [
-    ["-c", "--compile", "compile to JavaScript and save as .js files"],
-    ["-p", "--print"  , "print out the compiled JavaScript"          ],
-    ["-v", "--version", "display the version number"                 ],
+    ["-c", "--compile", "compile to JavaScript and save as .js files" ],
+    ["-e", "--eval"   , "pass a string from the command line as input"],
+    ["-p", "--print"  , "print out the compiled JavaScript"           ],
+    ["-v", "--version", "display the version number"                  ],
   ];
   var BANNER = "Usage: coffeecollider [options] path/to/script.coffee";
   
@@ -18,6 +19,9 @@
   
   parser.on("compile", function() {
     opts.compile = true;
+  });
+  parser.on("eval", function() {
+    opts.eval = true;
   });
   parser.on("print", function() {
     opts.print = true;
@@ -28,24 +32,31 @@
   
   var client = cc.CoffeeCollider();
   var args   = parser.parse(process.argv.slice(2));
-  var filepath = args[0] || "";
-  var src = null, code = null;
+  var arg0 = args[0] || "";
+  var filepath = null, src = null, code = null;
   
-  if (filepath && /\.coffee$/.test(filepath)) {
-    src  = fs.readFileSync(filepath).toString();
-    code = client.compile(src.trim());
+  if (arg0) {
+    if (opts.eval) {
+      src = arg0;
+    } else if (/\.coffee$/.test(arg0) && fs.existsSync(arg0)) {
+      filepath = arg0;
+      src  = fs.readFileSync(filepath).toString();
+    }
   }
   
   if (opts.version) {
     console.log("CoffeeCollider version " + cc.version);
   }
   if (src) {
+    code = client.compile(src.trim());
     if (opts.print) {
       console.log(code);
     }
     if (opts.compile) {
-      filepath = filepath.replace(/\.coffee$/, ".js");
-      fs.writeFileSync(filepath, code);
+      if (filepath) {
+        filepath = filepath.replace(/\.coffee$/, ".js");
+        fs.writeFileSync(filepath, code);
+      }
     } else {
       client.execute(code, {lang:"js"}).play();
     }
