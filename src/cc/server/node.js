4a7dc1203bd851fd581aded88d5f4417ebaf8274
mohayonao <mohayonao@gmail.com>
2013-12-30 12:23:45 +0900

    sort

--- src/cc/server/node.js
+++ src/cc/server/node.js
@@ -7,211 +7,6 @@ define(function(require, exports, module) {
   var graphFunc  = [];
   var doneAction = [];
   
-  graphFunc[C.ADD_TO_HEAD] = function(child, addThisOne) {
-    if (child.nodeId !== 0 && addThisOne instanceof Group) {
-      child.prev = null;
-      child.next = addThisOne.head;
-      if (addThisOne.head) {
-        addThisOne.head.prev = child;
-        addThisOne.head = child;
-      } else {
-        addThisOne.head = addThisOne.tail = child;
-      }
-      child.parent = addThisOne;
-    }
-  };
-  graphFunc[C.ADD_TO_TAIL] = function(child, addThisOne) {
-    if (child.nodeId !== 0 && addThisOne instanceof Group) {
-      child.prev = addThisOne.tail;
-      child.next = null;
-      if (addThisOne.tail) {
-        addThisOne.tail.next = child;
-        addThisOne.tail = child;
-      } else {
-        addThisOne.head = addThisOne.tail = child;
-      }
-      child.parent = addThisOne;
-    }
-  };
-  graphFunc[C.ADD_BEFORE] = function(node, beforeThisOne) {
-    if (node.nodeId !== 0 && beforeThisOne.parent) {
-      node.parent = beforeThisOne.parent;
-      node.prev = beforeThisOne.prev;
-      node.next = beforeThisOne;
-      
-      if (beforeThisOne.prev) {
-        beforeThisOne.prev.next = node;
-      } else {
-        node.parent.head = node;
-      }
-      beforeThisOne.prev = node;
-    }
-  };
-  graphFunc[C.ADD_AFTER] = function(node, afterThisOne) {
-    if (node.nodeId !== 0 && afterThisOne.parent) {
-      node.parent = afterThisOne.parent;
-      node.prev = afterThisOne;
-      node.next = afterThisOne.next;
-      
-      if (afterThisOne.next) {
-        afterThisOne.next.prev = node;
-      } else {
-        node.parent.tail = node;
-      }
-      afterThisOne.next = node;
-    }
-  };
-  graphFunc[C.REPLACE] = function(node, replaceThisOne) {
-    if (node.node !== 0 && replaceThisOne.parent) {
-      var parent = node.parent = replaceThisOne.parent;
-      node.prev = replaceThisOne.prev;
-      node.next = replaceThisOne.next;
-      
-      if (node instanceof Group) {
-        node.head = replaceThisOne.head;
-        node.tail = replaceThisOne.tail;
-      }
-      
-      if (replaceThisOne.prev) {
-        replaceThisOne.prev.next = node;
-      }
-      if (replaceThisOne.next) {
-        replaceThisOne.next.prev = node;
-      }
-      if (parent.head === replaceThisOne) {
-        parent.head = node;
-      }
-      if (parent.tail === replaceThisOne) {
-        parent.tail = node;
-      }
-    }
-  };
-
-  // do nothing when the UGen is finished
-  doneAction[0] = null;
-  
-  // pause the enclosing synth, but do not free it
-  doneAction[1] = function(node) {
-    node.running = false;
-  };
-  
-  // free the enclosing synth
-  doneAction[2] = function(node) {
-    node.end();
-  };
-
-  // free both this synth and the preceding node
-  doneAction[3] = function(node) {
-    if (node.prev) {
-      node.prev.end();
-    }
-    node.end();
-  };
-
-  // free both this synth and the following node
-  doneAction[4] = function(node) {
-    if (node.next) {
-      node.next.end();
-    }
-    node.end();
-  };
-  
-  // free this synth; if the preceding node is a group then do g_freeAll on it, else free it
-  doneAction[5] = function(node) {
-    var prev = node.prev;
-    if (prev instanceof Group) {
-      prev.endAll();
-    } else if (prev) {
-      prev.end();
-    }
-    node.end();
-  };
-  
-  // free this synth; if the following node is a group then do g_freeAll on it, else free it
-  doneAction[6] = function(node) {
-    var next = node.next;
-    node.end();
-    if (next instanceof Group) {
-      next.endAll();
-    } else if (next) {
-      next.end();
-    }
-  };
-
-  // free this synth and all preceding nodes in this group
-  doneAction[7] = function(node) {
-    var prev;
-    while (node) {
-      prev = node.prev;
-      node.end();
-      node = prev;
-    }
-  };
-  
-  // free this synth and all following nodes in this group
-  doneAction[8] = function(node) {
-    var next;
-    while (node) {
-      next = node.next;
-      node.end();
-      node = next;
-    }
-  };
-  
-  // free this synth and pause the preceding node
-  doneAction[9] = function(node) {
-    if (node.prev) {
-      node.prev.running = false;
-    }
-    node.end();
-  };
-  
-  // free this synth and pause the following node
-  doneAction[10] = function(node) {
-    if (node.next) {
-      node.next.running = false;
-    }
-    node.end();
-  };
-  
-  // free this synth and if the preceding node is a group then do g_deepFree on it, else free it
-  doneAction[11] = function(node) {
-    var prev = node.prev;
-    if (prev instanceof Group) {
-      prev.endDeep();
-    } else if (prev) {
-      prev.end();
-    }
-    node.end();
-  };
-
-  // free this synth and if the following node is a group then do g_deepFree on it, else free it
-  doneAction[12] = function(node) {
-    var next = node.next;
-    if (next instanceof Group) {
-      next.endDeep();
-    } else if (next) {
-      next.end();
-    }
-    node.end();
-  };
-  
-  // free this synth and all other nodes in this group (before and after)
-  doneAction[13] = function(node) {
-    var next;
-    node = node.parent.head;
-    while (node) {
-      next = node.next;
-      node.end();
-      node = next;
-    }
-  };
-  
-  // free the enclosing group and all nodes within it (including this synth)
-  doneAction[14] = function(node) {
-    node.parent.endDeep();
-  };
-  
   var Node = (function() {
     function Node(world, nodeId) {
       this.world   = world;
@@ -393,6 +188,212 @@ define(function(require, exports, module) {
     
     return Synth;
   })();
+  
+  graphFunc[C.ADD_TO_HEAD] = function(child, addThisOne) {
+    if (child.nodeId !== 0 && addThisOne instanceof Group) {
+      child.prev = null;
+      child.next = addThisOne.head;
+      if (addThisOne.head) {
+        addThisOne.head.prev = child;
+        addThisOne.head = child;
+      } else {
+        addThisOne.head = addThisOne.tail = child;
+      }
+      child.parent = addThisOne;
+    }
+  };
+  graphFunc[C.ADD_TO_TAIL] = function(child, addThisOne) {
+    if (child.nodeId !== 0 && addThisOne instanceof Group) {
+      child.prev = addThisOne.tail;
+      child.next = null;
+      if (addThisOne.tail) {
+        addThisOne.tail.next = child;
+        addThisOne.tail = child;
+      } else {
+        addThisOne.head = addThisOne.tail = child;
+      }
+      child.parent = addThisOne;
+    }
+  };
+  graphFunc[C.ADD_BEFORE] = function(node, beforeThisOne) {
+    if (node.nodeId !== 0 && beforeThisOne.parent) {
+      node.parent = beforeThisOne.parent;
+      node.prev = beforeThisOne.prev;
+      node.next = beforeThisOne;
+      
+      if (beforeThisOne.prev) {
+        beforeThisOne.prev.next = node;
+      } else {
+        node.parent.head = node;
+      }
+      beforeThisOne.prev = node;
+    }
+  };
+  graphFunc[C.ADD_AFTER] = function(node, afterThisOne) {
+    if (node.nodeId !== 0 && afterThisOne.parent) {
+      node.parent = afterThisOne.parent;
+      node.prev = afterThisOne;
+      node.next = afterThisOne.next;
+      
+      if (afterThisOne.next) {
+        afterThisOne.next.prev = node;
+      } else {
+        node.parent.tail = node;
+      }
+      afterThisOne.next = node;
+    }
+  };
+  graphFunc[C.REPLACE] = function(node, replaceThisOne) {
+    if (node.node !== 0 && replaceThisOne.parent) {
+      var parent = node.parent = replaceThisOne.parent;
+      node.prev = replaceThisOne.prev;
+      node.next = replaceThisOne.next;
+      
+      if (node instanceof Group) {
+        node.head = replaceThisOne.head;
+        node.tail = replaceThisOne.tail;
+      }
+      
+      if (replaceThisOne.prev) {
+        replaceThisOne.prev.next = node;
+      }
+      if (replaceThisOne.next) {
+        replaceThisOne.next.prev = node;
+      }
+      if (parent.head === replaceThisOne) {
+        parent.head = node;
+      }
+      if (parent.tail === replaceThisOne) {
+        parent.tail = node;
+      }
+    }
+  };
+  
+  // do nothing when the UGen is finished
+  doneAction[0] = null;
+  
+  // pause the enclosing synth, but do not free it
+  doneAction[1] = function(node) {
+    node.running = false;
+  };
+  
+  // free the enclosing synth
+  doneAction[2] = function(node) {
+    node.end();
+  };
+
+  // free both this synth and the preceding node
+  doneAction[3] = function(node) {
+    if (node.prev) {
+      node.prev.end();
+    }
+    node.end();
+  };
+
+  // free both this synth and the following node
+  doneAction[4] = function(node) {
+    if (node.next) {
+      node.next.end();
+    }
+    node.end();
+  };
+  
+  // free this synth; if the preceding node is a group then do g_freeAll on it, else free it
+  doneAction[5] = function(node) {
+    var prev = node.prev;
+    if (prev instanceof Group) {
+      prev.endAll();
+    } else if (prev) {
+      prev.end();
+    }
+    node.end();
+  };
+  
+  // free this synth; if the following node is a group then do g_freeAll on it, else free it
+  doneAction[6] = function(node) {
+    var next = node.next;
+    node.end();
+    if (next instanceof Group) {
+      next.endAll();
+    } else if (next) {
+      next.end();
+    }
+  };
+
+  // free this synth and all preceding nodes in this group
+  doneAction[7] = function(node) {
+    var prev;
+    while (node) {
+      prev = node.prev;
+      node.end();
+      node = prev;
+    }
+  };
+  
+  // free this synth and all following nodes in this group
+  doneAction[8] = function(node) {
+    var next;
+    while (node) {
+      next = node.next;
+      node.end();
+      node = next;
+    }
+  };
+  
+  // free this synth and pause the preceding node
+  doneAction[9] = function(node) {
+    if (node.prev) {
+      node.prev.running = false;
+    }
+    node.end();
+  };
+  
+  // free this synth and pause the following node
+  doneAction[10] = function(node) {
+    if (node.next) {
+      node.next.running = false;
+    }
+    node.end();
+  };
+  
+  // free this synth and if the preceding node is a group then do g_deepFree on it, else free it
+  doneAction[11] = function(node) {
+    var prev = node.prev;
+    if (prev instanceof Group) {
+      prev.endDeep();
+    } else if (prev) {
+      prev.end();
+    }
+    node.end();
+  };
+
+  // free this synth and if the following node is a group then do g_deepFree on it, else free it
+  doneAction[12] = function(node) {
+    var next = node.next;
+    if (next instanceof Group) {
+      next.endDeep();
+    } else if (next) {
+      next.end();
+    }
+    node.end();
+  };
+  
+  // free this synth and all other nodes in this group (before and after)
+  doneAction[13] = function(node) {
+    var next;
+    node = node.parent.head;
+    while (node) {
+      next = node.next;
+      node.end();
+      node = next;
+    }
+  };
+  
+  // free the enclosing group and all nodes within it (including this synth)
+  doneAction[14] = function(node) {
+    node.parent.endDeep();
+  };
+  
   
   cc.createServerRootNode = function(world) {
     return new Group(world, 0, 0, 0);
